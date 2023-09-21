9a7b9bdf2027dbb28d619220dd53c404ea358825
VitorCunha07 <144135895+VitorCunha07@users.noreply.github.com>
2023-09-21 17:44:46 -0300

    Add files via upload

---
+++ botoes.js
@@ -0,0 +1,17 @@
+const btn1 = document.querySelector('.btn1')
+
+btn1.addEventListener('click', function(){
+    alert("A pagina que você deseja acessar não se encontra disponivel no momento");
+});
+
+const btn2 = document.querySelector('.btn2')
+
+// btn2.addEventListener('click', function(){
+//     alert("Para mais informações, acesse: https://powertrip.live");
+// });
+
+const btn3 = document.querySelector('.btn3')
+
+btn3.addEventListener('click', function(){  
+    alert("Email para contato: vitorcunha1512@gmail.com");
+});
